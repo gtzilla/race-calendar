b4e57ce67c522e4319df312e5c753b039b846d8a
gtzilla <gregory.tomlinson@gmail.com>
2022-03-06 02:12:22 +0000

    Deploying to gh-pages from @ gtzilla/race-calendar@243d4dfe40a29c21eb3c890375d9d6ec16f14ae4 🚀

--- pages/about.jsx
+++
@@ -1,9 +0,0 @@
-const About = () => {
-  return (
-    <div className="mt-3">
-      <p className="display-4 text-center">Race Calendar</p>
-      <p className="text-center">U.S.A. Alleycat races. Hosting a Race? Send races and events to <a href="https://www.instagram.com/brokeshutter/">ig@brokeshutter</a></p>
-    </div>
-  )
-}
-export default About
