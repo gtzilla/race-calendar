7c1fc081abbbdbe3ebca3782e4cf7c9992742cf8
gtomlinson <gregory.tomlinson@disneystreaming.com>
2022-05-08 10:28:29 -0400

    work smithing

--- pages/about.jsx
+++ pages/about.jsx
@@ -7,8 +7,8 @@ const About = () => {
       <p className="display-4 ">About Race Calendar</p>
       <p>
         A directory of the best <Link href="https://en.wikipedia.org/wiki/Alleycat_race">Alleycat</Link> races in North America. 
-        Race Calendar is developed in New York City as way to keep track of the 
-        best races for athletes, participants and spectators to keep informed on Alleycats
+        Race Calendar is developed in New York City to keep track of the 
+        best races for athletes, participants and spectators. It's great to keep informed on Alleycats
         throughout U.S.A, Mexico and Canada.
       </p>
 
@@ -20,7 +20,7 @@ const About = () => {
         events to <ProfileLink username='brokeshutter' />. Listings your race is free.
       </p>
 
-      <h3>International Races?</h3>
+      <h3>International Races</h3>
       <p>Please get in touch with <ProfileLink username='brokeshutter' /> if 
       you are hosting a race outside the United States / North America.</p>
     </div>
